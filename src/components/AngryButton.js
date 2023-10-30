543c8039a27af721a0afbb7608efa7a1264e64ee
mxtchjohnston <mitchjohnston99@gmail.com>
2023-10-30 16:20:01 -0700

    use reducer refactor

--- src/components/AngryButton.js
+++ src/components/AngryButton.js
@@ -1,12 +1,10 @@
-import { useState } from "react";
+import { useReducer } from "react";
 
 function AngryButton(props){
-  const [anger, setAnger] = useState(0);
-
-  const handleClick = amt => () => anger < 1 ? setAnger(amt + anger) : setAnger(0);
+  const [anger, dispatch] = useReducer((anger, action) => anger > 1 ? 0 : anger + action, 0);
 
   return (
-    <button style={{ backgroundColor: `rgba(255,0,0,${anger})`}} className="AngryButton" onClick={handleClick(0.1)}>
+    <button style={{ backgroundColor: `rgba(255,0,0,${anger})`}} className="AngryButton" onClick={() => dispatch(0.1)}>
       {anger < 1 && <span>Don't click me too much! </span>}
       {anger > 1 && <span>Rawr!</span>}
     </button>
